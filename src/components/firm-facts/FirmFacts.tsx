4853c6a3816c95a9fa2023b680dae388b038174b
skritnost <andrei@scal.io>
2025-01-10 13:13:11 -0300

    feat: update FirmFacts component template + breakpoints

--- src/components/firm-facts/FirmFacts.tsx
+++ src/components/firm-facts/FirmFacts.tsx
@@ -3,9 +3,29 @@ import * as style from './FirmFacts.module.scss';
 export const FirmFacts = ({variant}: { variant: 'default' }) => {
     return (
         <div className={variant === 'default' ? style.firmFacts : null}>
-            <div className={style.close} />
-            <div>
-
+            <div className={style.content}>
+                <div className={style.close}/>
+                <h3 className={style.title}>
+                    Firm Facts
+                </h3>
+                <div className={style.divider} />
+                <div className={style.cards}>
+                    <div>
+                        Any
+                    </div>
+                    <div>
+                        Any1
+                    </div>
+                    <div>
+                        Any2
+                    </div>
+                    <div>
+                        Any3
+                    </div>
+                    <div>
+                        Any4
+                    </div>
+                </div>
             </div>
         </div>
     );
